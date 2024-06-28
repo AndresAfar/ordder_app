ab87b3612ec0fedcb2149ab47bcbad70a717fd91
AndresAfar <afar2503@gmail.com>
2024-06-28 10:52:13 -0500

    Se modifican y agregan rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,31 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/products', function () {
+    return view('products');
+})->middleware(['auth', 'verified'])->name('products');
+
+Route::get('/orders', function () {
+    return view('orders.order-index');
+})->middleware(['auth', 'verified'])->name('orders');
+
+/*Route::get('/staff', function () {
+    return view('staff.staff-index');
+})->middleware(['auth', 'verified'])->name('staff');
+*/
+
+// vistas staff
+Route::middleware('auth')->group(function () {
+    Route::get('/staff', [StaffController::class, 'index'])->name('staff');
+    Route::get('/create/staff', [StaffController::class, 'create'])->name('staff.create');
+    Route::get('/staff/modified', [StaffController::class, 'edit'])->name('staff.edit');
+
+    Route::post('/create/staff', [StaffController::class, 'store'])->name('staff.store');
+    Route::patch('/staff', [StaffController::class, 'update'])->name('staff.update');
+    Route::delete('/staff', [StaffController::class, 'destroy'])->name('staff.destroy');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
